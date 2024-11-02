7351c639f524b81a1f7eab6d6e6cc2069a362ca4
seohyun-lee <applemint2003@gmail.com>
2024-11-03 01:49:40 +0900

    feat: cors 허용할 포트 추가

--- src/main/java/com/unidthon/jabuhae/global/config/SecurityConfig.java
+++ src/main/java/com/unidthon/jabuhae/global/config/SecurityConfig.java
@@ -48,7 +48,9 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:3000",
-                "https://localhost:3000"));
+                "https://localhost:3000",
+                "http://localhost:5173",
+                "http://localhost:5174"));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"));
         configuration.addAllowedHeader("*");
