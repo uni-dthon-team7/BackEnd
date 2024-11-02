25e6a3c4438a93afbfe7f1d7f8a0f8091da4bc24
LHC0312 <chan050312@gmail.com>
2024-11-03 02:57:12 +0900

    feat: CommentDto 추가중

---
+++ src/main/java/com/unidthon/jabuhae/domain/dto/response/CommentResponseDto.java
@@ -0,0 +1,20 @@
+package com.unidthon.jabuhae.domain.dto.response;
+
+import com.unidthon.jabuhae.domain.entity.Recipe;
+import com.unidthon.jabuhae.domain.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class CommentResponseDto {
+  @Builder
+  @Getter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class ResultDto {
+    private Long commentId;
+    private String content;
+    private User user;
+  }
+}
---
+++ src/main/java/com/unidthon/jabuhae/domain/service/RecipeService.java
@@ -0,0 +1,5 @@
+package com.unidthon.jabuhae.domain.service;
+
+public class RecipeService {
+
+}
